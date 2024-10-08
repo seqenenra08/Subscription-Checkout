ced73e8008e69a482d56754df8b90b4cf76a815f
seqenenra08 <juandavidalvarez08@gmail.com>
2024-10-07 21:47:38 -0500

    Try rotateY in cards

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <>
     <section className="w-full h-full flex justify-center items-center">
-      <div className="w-[350px] h-[550px] backdrop"></div>
+      <div className="w-[350px] h-[550px] backdrop border-b-2"></div>
+      <div className="w-[350px] h-[550px] backdrop ml-6 mr-6"></div>
+      <div className="w-[350px] h-[550px] backdrop border-b-2"></div>
     </section>
     </>
   )
